5ce9d77dddc037d1d7b855280a47615660e9d711
vaibhav <gatne.vaibhav@gmail.com>
2021-09-05 19:21:28 +0530

    test folder added

---
+++ test/java/test.java
@@ -0,0 +1,37 @@
+add line 
+123
+23123
+
+if(browser=="chrome")
+{
+	system.out.print("it works fine");
+}
+else {
+	system.out.print("it works fine on IE");
+}
+
+
+if(browser=="chrome")
+{
+	system.out.print("it works fine");
+}
+else {
+	system.out.print("it works fine on IE");
+}
+
+if(browser=="chrome")
+{
+	system.out.print("it works fine");
+}
+else {
+	system.out.print("it works fine on IE");
+}
+
+if(browser=="chrome")
+{
+	system.out.print("it works fine");
+}
+else {
+	system.out.print("it works fine on IE");
+}
+
